6fe86babb3acf8d4d32315f92d374d9dd21df4ed
ashwanisinghel <singh111ashwani@gmail.com>
2022-12-29 12:00:01 +0530

    welcome to node using server

--- first-app.js
+++ first-app.js
@@ -1,7 +1,13 @@
 const http=require('http');
 
 const server=http.createServer((req,res)=>{
-    console.log(req)
+    console.log(req.url,req.method,req.headers);
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>')
+    res.write('<head><title>First Page</title><head>')
+    res.write('<body><h1>Hi from node js</h1></body>')
+    res.write('</html>')
+
 })
 
 server.listen(4000);
