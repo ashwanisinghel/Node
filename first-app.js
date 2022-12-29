ecb1af4d178d5a3fd7e46346bb8a333291a4eedd
ashwanisinghel <singh111ashwani@gmail.com>
2022-12-29 12:29:01 +0530

    assignment 7

--- first-app.js
+++ first-app.js
@@ -1,13 +1,34 @@
 const http=require('http');
 
 const server=http.createServer((req,res)=>{
+    const url=req.url;
     console.log(req.url,req.method,req.headers);
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>')
-    res.write('<head><title>First Page</title><head>')
-    res.write('<body><h1>Hi from node js</h1></body>')
-    res.write('</html>')
-
+    // console.log('Ashwani Singh')
+    if(url==='/home'){
+        res.write('<html>')
+        res.write('<head><title>First Page</title><head>')
+        res.write('<body><h1>This is homepage</h1></body>')
+        res.write('</html>')
+        return res.end();
+    }else if(url==='/about'){
+        res.write('<html>')
+        res.write('<head><title>First Page</title><head>')
+        res.write('<body><h1>welcome to about page</h1></body>')
+        res.write('</html>')
+        return res.end();
+    }else if(url==='/about'){
+        res.write('<html>')
+        res.write('<head><title>First Page</title><head>')
+        res.write('<body><h1>welcome to about page</h1></body>')
+        res.write('</html>')
+        return res.end();
+    }else if(url==='/Node'){
+        res.write('<html>')
+        res.write('<head><title>Node Page</title><head>')
+        res.write('<body><h1>Welcome to my node>js Project</h1></body>')
+        res.write('</html>')
+        return res.end();
+    }
 })
 
 server.listen(4000);
