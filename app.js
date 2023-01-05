861e25e0a538661c3592b2047e45ff1d95b5a7bb
ashwanisinghel <singh111ashwani@gmail.com>
2023-01-05 10:31:52 +0530

    express assignment 2

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const http=require('http');
-const routes=require('./route')
+const express=require('express');
 
-const server=http.createServer(routes.handeler);
+const app=express();
 
-server.listen(3000);
+app.use((req,res,next)=>{
+    console.log('express chal rha hai bhai');
+    res.send('<h1>Jai ho BABA ki<h1>');
+    next();
+})
+app.use((req,res,next)=>{
+    console.log('2nd middleware be like swagat nahi karoge hamara');
+})
+
+app.listen(4000);
--- new_app.js
+++
@@ -1,8 +0,0 @@
-const http=require('http');
-const fs=require('fs');
-
-const server=http.createServer((req,res)=>{
-    console.log(req)
-})
-
-server.listen(4000)
