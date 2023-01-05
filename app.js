37e0adafee45bef9d9427d5d9c42d8ef7becd95a
ashwanisinghel <singh111ashwani@gmail.com>
2023-01-05 12:02:17 +0530

    express assignment 3

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express=require('express');
+const bodyParser=require('body-parser')
 
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log('express chal rha hai bhai');
-    res.send('<h1>Jai ho BABA ki<h1>');
+app.use(bodyParser.urlencoded({extended:false}))
+app.use('/',(req,res,next)=>{
+    console.log('this will always come')
     next();
 })
-app.use((req,res,next)=>{
-    console.log('2nd middleware be like swagat nahi karoge hamara');
+
+app.use('/product',(req,res,next)=>{
+    res.send("<form action='add-product' method='post'><input type='text', name='product'><input type='text' name='size'><button type='submit'>Add product</button></form>")
+})
+app.use('/add-product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/product')
 })
 
 app.listen(4000);
